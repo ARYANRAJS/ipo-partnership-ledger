e78840a2ef61d9c46d786547077671edb96428dd
ARYANRAJS <106547134+ARYANRAJS@users.noreply.github.com>
2026-08-19 10:48:06 +0530

    Add vite.config.js for Vercel/Render free deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-v3-${Date.now()}.js`,
+        chunkFileNames: `assets/[name]-v3-${Date.now()}.js`,
+        assetFileNames: `assets/[name]-v3-[hash].[ext]`
+      }
+    }
+  },
   server: {
     port: 3000,
     open: false
