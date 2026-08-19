c8f2537c90d8b3a3ff338d7a2024e367a375b725
ARYANRAJS <106547134+ARYANRAJS@users.noreply.github.com>
2026-08-19 11:27:01 +0530

    Add api/live-ipos/stream.js for Vercel/Render free deployment

--- api/live-ipos/stream.js
+++ api/live-ipos/stream.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -46,4 +46,4 @@ export default async function handler(req, res) {
 
   res.write(`data: ${JSON.stringify(fallbackPayload)}\n\n`);
   return res.end();
-}
+};
